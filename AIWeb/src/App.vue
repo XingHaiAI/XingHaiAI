f8aaab317bdd5905fa014d0b0ba4e3c72f30e927
WuYou <503839394@qq.com>
2018-07-05 11:16:47 +0800

    修改了app.vue和homepage，去除最下面的横拉条

--- AIWeb/src/App.vue
+++ AIWeb/src/App.vue
@@ -66,7 +66,7 @@
     <router-view></router-view>
 
         <div id="footer" style="margin: 0px;padding: 0px;width: 100%" v-if="$route.path!='/login'&&$route.path!='/signin'">
-          <img style="width: 100vw;margin: 0px;padding: 0px" src="./assets/联系我们.jpg"/>
+          <img style="width: 100%;margin: 0px;padding: 0px" src="./assets/联系我们.jpg"/>
         </div>
 
   </div>
