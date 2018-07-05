5c1d883e0b562680b280a69d9972bef6b837f377
WuYou <503839394@qq.com>
2018-07-05 15:46:18 +0800

    修改了联系我们，但壁纸左方有白边问题未改

--- AIWeb/src/DocumentCenter/ContactBussiness.vue
+++ AIWeb/src/DocumentCenter/ContactBussiness.vue
@@ -1,389 +1,390 @@
 <template>
   <html>
-<body>
-<div class="bg">
+  <body>
+  <div class="bg">
     <div class="head_content">
-        <div class="title">联系我们</div>
-        <div class="content">CONTACT  WITH  US</div>
+      <div class="title">联系我们</div>
+      <div class="content">CONTACT  WITH  US</div>
     </div>
     <div class="link_group">
-        <div class="title">
-            <div class="decorate_bar"></div>
-            <div class="word">常见问题</div>
+      <div class="title">
+        <div class="decorate_bar"></div>
+        <div class="word">常见问题</div>
+      </div>
+      <div class="link_content">
+        <div class="title">售前咨询</div>
+        <div class="content">
+          <div class="word">如何接入API？</div><br/>
+          <div class="word">API如何收费？</div><br/>
+          <div class="word">返回值报错怎么办？</div>
         </div>
-        <div class="link_content">
-            <div class="title">售前咨询</div>
-            <div class="content">
-                <div class="word">如何接入API？</div><br/>
-                <div class="word">API如何收费？</div><br/>
-                <div class="word">返回值报错怎么办？</div>
-            </div>
-            <div class="title">商务咨询</div>
-            <div class="content">
-                <div class="word">有什么合作方案？</div>
-            </div>
+        <div class="title">商务咨询</div>
+        <div class="content">
+          <div class="word">有什么合作方案？</div>
         </div>
+      </div>
     </div>
     <div class="from_group">
-        <div class="decorate_bar">提交反馈</div>
-        <div class="item_group">
-            <div class="before_buy">
-                <div class="icon_1"></div>
-                <div class="word"><router-link to="/ContactUs" style="text-decoration: none;color: black">售前咨询</router-link></div>
-            </div>
-            <div class="business">
-                <div class="icon_2"></div>
-                <div class="word"><router-link to="/ContactBussiness" style="text-decoration: none;color: black">商务咨询</router-link></div>
-            </div>
-            <div class="question">
-                <div class="icon_3"></div>
-                <div class="word"><router-link to="/ContactProblem" style="text-decoration: none;color: black">问题反馈</router-link></div>
-            </div>
+      <div class="decorate_bar">提交反馈</div>
+      <div class="item_group">
+        <div class="before_buy">
+          <div class="icon_1"></div>
+          <div class="word"><router-link to="/ContactUs" style="text-decoration: none;color: black">售前咨询</router-link></div>
         </div>
-        <div class="inro">
-        	客服人员将解答您在购买产品前的一切疑问。请您在下方提供<br/>您的联系方式，并描述您的问题。
+        <div class="business">
+          <div class="icon_2"></div>
+          <div class="word"><router-link to="/ContactBussiness" style="text-decoration: none;color: black">商务咨询</router-link></div>
         </div>
-        <div class="input_group">
-            <div class="input_item_left">
-                <input class="input" type="text" placeholder="姓名"/><span style="color:red;"> *</span>
-                <input class="input" type="text" placeholder="联系电话"/><span style="color:red;"> *</span>
-                <input class="input" type="text" placeholder="电子邮箱"/><span style="color:red;"> *</span>
-                <input class="input" type="text" placeholder="所属公司机构"/>
-            </div>
-            <textarea class="input_textarea" placeholder="请描述您的问题。"></textarea>
+        <div class="question">
+          <div class="icon_3"></div>
+          <div class="word"><router-link to="/ContactProblem" style="text-decoration: none;color: black">问题反馈</router-link></div>
         </div>
-        <div class="check_box">
-            <input class="box" type="checkbox"/>
-            <div class="word">我已阅读并同意<span class="xinghai"> 《兴海物联隐私政策》 </span></div>
+      </div>
+      <div class="inro">
+        客服人员将解答您在购买产品前的一切疑问。请您在下方提供<br/>您的联系方式，并描述您的问题。
+      </div>
+      <div class="input_group">
+        <div class="input_item_left">
+          <input class="input" type="text" placeholder="姓名"/><span style="color:red;"> *</span>
+          <input class="input" type="text" placeholder="联系电话"/><span style="color:red;"> *</span>
+          <input class="input" type="text" placeholder="电子邮箱"/><span style="color:red;"> *</span>
+          <input class="input" type="text" placeholder="所属公司机构"/>
         </div>
-        <div class="submit">提交</div>
+        <textarea class="input_textarea" placeholder="请描述您的问题。"></textarea>
+      </div>
+      <div class="check_box">
+        <input class="box" type="checkbox"/>
+        <div class="word">我已阅读并同意<span class="xinghai"> 《兴海物联隐私政策》 </span></div>
+      </div>
+      <div class="submit">提交</div>
     </div>
-</div>
-</body>
+  </div>
+  </body>
   </html>
 </template>
 <script>
-export default {
-  name: 'ContactBussiness'
-}
+  export default {
+    name: 'ContactUs'
+  }
 </script>
 
 <style scoped>
-#ContactUs{
-  background-color: blue;
-  background-image: url("../assets/联系我们/bg.jpg");
-  background-size: 100% 100%;
-  width: 13.66rem;
-  height: 35vh;
-  margin: 0;
-  padding: 0;
-  display: flex;
-  flex-direction: column;
-  font-size: 62.5%;
-}
-body,form,div,ul,ol,li,h1,h2,h3,h4,h5,h6,table,tr,th,td,p,input,dl,dt,dd,ul,ol,li,input,textarea { font-family:"微软雅黑"!important;}
-.background {
-  width:100%;
-  height:100%;
-}
-div{
-  margin: 0;
-  padding: 0;
-  vertical-align: top;
-}
-.word_head{
-  margin-top: 3vh;
-  margin-left: -0.888rem;
-  text-align: center;
-  color: white;
-  font-size: 0.2rem;
-}
-.line{
-  align-self: center;
-  margin-top: 2vh;
-}
-.step_line{
-  margin-top: 1.5vh;
-}
-.icon_1{
-  background-image: url("../assets/联系我们/icon1.png");
-  margin-top:0.7vh;
-  background-size: 100% 100%;
-  display: inline-block;
-  width: 3.5vh;
-  height: 3.5vh;
-}
-.icon_2{
-  background-image: url("../assets/联系我们/icon2.png");
-  background-size: 100% 100%;
-  margin-top:0.7vh;
-  display: inline-block;
-  width: 3.5vh;
-  height: 3.5vh;
-}
-.icon_3{
-  background-image: url("../assets/联系我们/icon3.png");
-  background-size: 100% 100%;
-  margin-top:0.7vh;
-  display: inline-block;
-  width: 3.5vh;
-  height: 3.5vh;
-}
-.word{
-  display: inline-block;
-  text-align: center;
-  color: black;
-  line-height: 3vh;
-  font-size: 0.2rem;
-  cursor:default;
-}
-.coloum_1{
-  display: inline-block;
-}
-.coloum_2{
-  margin-left: 0.41rem;
-  margin-bottom:5vh;
-  background-image: url("../assets/联系我们/pic1.jpg");
-  background-size: 100% 100%;
-  display: inline-block;
-  width: 10vh;
-  height: 10vh;
-}
-.inro{
-  height:10vh;
-  line-height:4vh;
-  font-size:0.2rem;
-  letter-spacing:0.2em;
-  margin-top:1vh;
-  margin-bottom:1vh;
-  background-image: url("../assets/pic/气泡.png");
-  background-size: 100% 100%;
-  align-self:center;
-}
-body{
-  margin: 0;
-  padding: 0;
-}
-.xinghai{
-  color:#2285ea;
-}
-div{
-  margin: 0;
-  padding: 0;
-  vertical-align: 80%;
-}
-.bg{
-  height: 221vh;
-  width: 13.66rem;
+  #ContactUs{
+    background-color: blue;
+    background-image: url("../assets/联系我们/bg.jpg");
+    background-size: 100% 100%;
+    width: 13.66rem;
+    height: 2.45rem;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    font-size: 62.5%;
+  }
+  body,form,div,ul,ol,li,h1,h2,h3,h4,h5,h6,table,tr,th,td,p,input,dl,dt,dd,ul,ol,li,input,textarea { font-family:"微软雅黑"!important;}
+  .background {
+    width:100%;
+    height:100%;
+  }
+  div{
+    margin: 0;
+    padding: 0;
+    vertical-align: top;
+  }
+  .word_head{
+    margin-top: 0.01rem;
+    margin-left: -0.888rem;
+    text-align: center;
+    color: white;
+    font-size: 0.2rem;
+  }
+  .line{
+    align-self: center;
+    margin-top: 0.02rem;
+  }
+  .step_line{
+    margin-top: 0.001rem;
+  }
+  .icon_1{
+    background-image: url("../assets/联系我们/icon1.png");
+    margin-top:0.05rem;
+    background-size: 100% 100%;
+    display: inline-block;
+    width: 0.03rem;
+    height: 0.03rem;
+  }
+  .icon_2{
+    background-image: url("../assets/联系我们/icon2.png");
+    background-size: 100% 100%;
+    margin-top:0.05rem;
+    display: inline-block;
+    width: 0.03rem;
+    height: 0.03rem;
+  }
+  .icon_3{
+    background-image: url("../assets/联系我们/icon3.png");
+    background-size: 100% 100%;
+    margin-top:0.05rem;
+    display: inline-block;
+    width: 0.03rem;
+    height: 0.03rem;
+  }
+  .word{
+    display: inline-block;
+    text-align: center;
+    color: black;
+    line-height: 0.3rem;
+    font-size: 0.2rem;
+    cursor:default;
+  }
+  .coloum_1{
+    display: inline-block;
+  }
+  .coloum_2{
+    margin-left: 0.41rem;
+    margin-bottom:0.05rem;
+    background-image: url("../assets/联系我们/pic1.jpg");
+    background-size: 100% 100%;
+    display: inline-block;
+    width: 0.10rem;
+    height: 0.10rem;
+  }
+  .inro{
+    height:1rem;
+    line-height:0.25rem;
+    font-size:0.2rem;
+    letter-spacing:0.2em;
+    margin-top:-1rem;
+    margin-bottom:0.5rem;
+    background-image: url("../assets/pic/气泡.png");
+    background-size: 100% 100%;
+    align-self:center;
+  }
+  body{
+    margin: 0;
+    padding: 0;
+  }
+  .xinghai{
+    color:#2285ea;
+  }
+  div{
+    margin: 0;
+    padding: 0;
+    vertical-align: 80%;
+  }
+  .bg{
+    height: 8rem;
+    width: 13.66rem;
 
-}
-.head_content{
-  width: 100%;
-  padding-top:10vh;
-  border: 0.02rem solid white;
-  height: 70.5vh;
-  background-image: url("../assets/pic/contact.png");
-  background-size: auto;
-  background-position: center;
-}
-.head_content .title{
-  font-size: 0.46rem;
-  font-weight:lighter;
-  color: white;
-  margin-top: 14vh;
-  margin-left: 1.639rem;
-  cursor:default;
-}
-.head_content .content{
-  font-size: 0.19rem;
-  font-weight:lighter;
-  color: white;
-  margin-left: 1.639rem;
-  width: 5.464rem;
-  margin-top: 2vh;
-  cursor:default;
-}
-.link_group{
-  float: left;
-  width: 3.415rem;
-  height: 159.5vh;
-  background-color: #fefeff;
-  box-shadow: 0 0.04rem 0.08rem 0 rgba(0, 0, 0, 0.2), 0 0.06rem 0.2rem 0 rgba(0, 0, 0, 0.19);
-}
-.from_group{
-  display: flex;
-  flex-direction: column;
-  float: left;
-  width: 10.245rem;
-  height: 100vh;
-}
-.link_group .decorate_bar{
-  margin-top: 10vh;
-  padding-top:0.5vh;
-  height: 3vh;
-  display: inline-block;
-  width: 0.041rem;
-  background-color: #2285ea;
-}
-.link_group .title .word{
-  line-height: 4vh;
-  display: inline-block;
-  font-size: 0.32rem;
-  font-weight: 500;
-  margin-top: 9vh;
-  padding-bottom:1vh;
+  }
+  .head_content{
+    width: 100%;
+    padding-top:0.5rem;
+    height: 5.40rem;
+    background-image: url("../assets/pic/contact.png");
+    background-size: 100% 5.40rem;
+    background-repeat: no-repeat;
+    min-width: 12rem;
+  }
+  .head_content .title{
+    font-size: 0.46rem;
+    font-weight:lighter;
+    color: white;
+    margin-top: 1.4rem;
+    margin-left: 1.639rem;
+    cursor:default;
+  }
+  .head_content .content{
+    font-size: 0.19rem;
+    font-weight:lighter;
+    color: white;
+    margin-left: 1.639rem;
+    width: 5.464rem;
+    margin-top: 0.02rem;
+    cursor:default;
+  }
+  .link_group{
+    float: left;
+    width: 3.415rem;
+    height: 9.55rem;
+    background-color: #fefeff;
+    box-shadow: 0 0.04rem 0.08rem 0 rgba(0, 0, 0, 0.2), 0 0.06rem 0.2rem 0 rgba(0, 0, 0, 0.19);
+  }
+  .from_group{
+    display: flex;
+    flex-direction: column;
+    float: left;
+    width: 10.245rem;
+    height: 5rem;
+  }
+  .link_group .decorate_bar{
+    margin-top: 0.9rem;
+    padding-top:0.05rem;
+    height: 0.32rem;
+    display: inline-block;
+    width: 0.041rem;
+    background-color: #2285ea;
+  }
+  .link_group .title .word{
+    line-height: 1.35rem;
+    display: inline-block;
+    font-size: 0.32rem;
+    font-weight: 500;
+    margin-top: 0.01rem;
+    padding-bottom:0.1rem;
 
-}
-.link_group .title{
-  margin-left:0.956rem;
-  cursor:default;
-}
-.link_content{
-  text-align: left;
-  cursor:default;
-  margin-left:0.137rem;
-}
-.link_content .title{
-  margin-left: 0.82rem;
-  text-decoration:underline;
-  font-size: 0.24rem;
-  margin-top: 3vh;
-  margin-bottom: 2vh;
-  font-weight: 400;
-  cursor:pointer;
-}
-.link_content .content{
-  margin-left: 0.915rem;
-  cursor:pointer;
-  margin-bottom:10vh;
-}
-.link_content .word{
-  margin-top: 2vh;
-  margin-bottom: 2vh;
-  margin-left: -0.137rem;
-  font-size: 0.22rem;
-  font-weight: 400;
-  cursor:pointer;
-}
-.from_group .decorate_bar{
-  width: 6.83rem;
-  height: 5vh;
-  margin-top: 10vh;
-  font-size:0.3rem;
-  text-align:center;
-  align-self: center;
-}
-.from_group .item_group{
-  margin-top: 5vh;
-  margin-left:1.776rem;
-  align-self: center;
-  width:9.56rem;
-}
-.item_group .word{
-  float: left;
-  line-height: 6vh;
-  margin-left: 0.11rem;
-  font-size:0.24rem;
-}
-.icon_1{
-  width: 0.273rem;
-  height: 4vh;
-  margin-right:0.0683rem;
-  float: left;
-  background-image: url("../assets/pic/icon1.png");
-  background-size: 100% 100%;
-  background-position: center;
-}
-.icon_2{
-  width: 0.273rem;
-  height: 4vh;
-  margin-right:0.0683rem;
-  float: left;
-  background-image: url("../assets/pic/icon2.png");
-  background-size: 100% 100%;
-  background-position: center;
-}
-.icon_3{
-  width: 0.273rem;
-  height: 4vh;
-  margin-right:0.0683rem;
-  float: left;
-  background-image: url("../assets/pic/icon3.png");
-  background-size: 100% 100%;
-  background-position: center;
-}
-.from_group .item_group .before_buy{
-  margin-right: 0.683rem;
-  margin-left: 0.683rem;
-  display: inline-block;
-}
-.from_group .item_group .business{
-  margin-right: 0.683rem;
-  margin-left: 0.683rem;
-  display: inline-block;
-}
-.from_group .item_group .question{
-  margin-right: 0.683rem;
-  margin-left: 0.683rem;
-  display: inline-block;
-}
-.input_group {
-  margin-top: 3vh;
-  align-self: center;
-}
-.input_item_left{
-  font-size: 0.2rem;
-  float: left;
-  width: 2.732rem;
-}
-.input{
-  border: none;
-  font-size: 0.2rem;
-  border: #D1D1D1 0.01rem solid;
-  border-radius: 0.05rem;
-  margin-bottom: 3vh;
-  height: 4.5vh;
-  width: 2.322rem;
-}
-.input_textarea{
-  border: none;
-  padding: 1.5vh;
-  font-size: 0.2rem;
-  border: #D1D1D1 0.01rem solid;
-  border-radius: 5%;
-  margin-left: 0.137rem;
-  height: 35.5vh;
-  width: 3.415rem;
-}
-.check_box{
-  font-size: 0.2rem;
-  font-weight: 100;
-  margin-top: 7vh;
-  align-self: center;
-  margin-left: -2.94rem;
-}
-.box{
-  margin-right: 0.137rem;
-  float: left;
-}
-.check_box .word{
-  float: left;
-}
-.submit{
-  text-align: center;
-  line-height: 6vh;
-  color: white;
-  font-size: 0.2rem;
-  font-weight: 200;
-  margin-top: 7vh;
-  align-self: center;
-  width: 1.367rem;
-  height: 6vh;
-  border-radius: 5%;
-  background-color: #3765d5;
-  cursor:pointer;
-}
+  }
+  .link_group .title{
+    margin-left:0.956rem;
+    cursor:default;
+  }
+  .link_content{
+    text-align: left;
+    cursor:default;
+    margin-left:0.137rem;
+  }
+  .link_content .title{
+    margin-left: 0.82rem;
+    text-decoration:underline;
+    font-size: 0.24rem;
+    margin-top: -0.5rem;
+    margin-bottom: 0.2rem;
+    font-weight: 400;
+    cursor:pointer;
+  }
+  .link_content .content{
+    margin-left: 0.915rem;
+    cursor:pointer;
+    margin-bottom:1.1rem;
+  }
+  .link_content .word{
+    margin-top: 0.02rem;
+    margin-bottom: 0.2rem;
+    margin-left: -0.137rem;
+    font-size: 0.22rem;
+    font-weight: 400;
+    cursor:pointer;
+  }
+  .from_group .decorate_bar{
+    width: 6.83rem;
+    height: 0.5rem;
+    margin-top: 1rem;
+    font-size:0.3rem;
+    text-align:center;
+    align-self: center;
+  }
+  .from_group .item_group{
+    margin-top: 0.3rem;
+    margin-left:0.3rem;
+    align-self: center;
+    width:85%;
+  }
+  .item_group .word{
+    float: left;
+    line-height: 0.36rem;
+    margin-left: 0rem;
+    font-size:0.24rem;
+  }
+  .icon_1{
+    width: 0.273rem;
+    height: 0.25rem;
+    margin-right:0.0983rem;
+    float: left;
+    background-image: url("../assets/pic/icon1.png");
+    background-size: 100% 100%;
+    background-position: center;
+  }
+  .icon_2{
+    width: 0.273rem;
+    height: 0.25rem;
+    margin-right:0.0983rem;
+    float: left;
+    background-image: url("../assets/pic/icon2.png");
+    background-size: 100% 100%;
+    background-position: center;
+  }
+  .icon_3{
+    width: 0.273rem;
+    height: 0.25rem;
+    margin-right:0.0983rem;
+    float: left;
+    background-image: url("../assets/pic/icon3.png");
+    background-size: 100% 100%;
+    background-position: center;
+  }
+  .from_group .item_group .before_buy{
+    margin-right: 0.683rem;
+    margin-left: 0.683rem;
+    display: inline-block;
+  }
+  .from_group .item_group .business{
+    margin-right: 0.683rem;
+    margin-left: 0.683rem;
+    display: inline-block;
+  }
+  .from_group .item_group .question{
+    margin-right: 0.683rem;
+    margin-left: 0.683rem;
+    display: inline-block;
+  }
+  .input_group {
+    margin-top: 0.03rem;
+    align-self: center;
+  }
+  .input_item_left{
+    font-size: 0.2rem;
+    float: left;
+    width: 2.732rem;
+  }
+  .input{
+    border: none;
+    font-size: 0.2rem;
+    border: #D1D1D1 0.01rem solid;
+    border-radius: 0.05rem;
+    margin-bottom: 0.35rem;
+    height: 0.3rem;
+    width: 2.322rem;
+  }
+  .input_textarea{
+    border: none;
+    padding: 0.05rem;
+    font-size: 0.2rem;
+    border: #D1D1D1 0.01rem solid;
+    border-radius: 5%;
+    margin-left: 0.137rem;
+    height: 2.55rem;
+    width: 3.415rem;
+  }
+  .check_box{
+    font-size: 0.2rem;
+    font-weight: 100;
+    margin-top: 0.4rem;
+    align-self: center;
+    margin-left: -1.94rem;
+  }
+  .box{
+    margin-top: 0.1rem;
+    margin-right: 0.137rem;
+    float: left;
+  }
+  .check_box .word{
+    float: left;
+  }
+  .submit{
+    text-align: center;
+    line-height: 0.4rem;
+    color: white;
+    font-size: 0.2rem;
+    font-weight: 200;
+    margin-top: 0.4rem;
+    align-self: center;
+    width: 1.367rem;
+    height: 0.4rem;
+    border-radius: 5%;
+    background-color: #3765d5;
+    cursor:pointer;
+  }
 
-.input_textarea::-webkit-input-placeholder { color: #B0B0B0;font-size:0.18rem;}
-.input::-webkit-input-placeholder { color: #B0B0B0;font-size:0.18rem;padding-left:0.068rem;}
+  .input_textarea::-webkit-input-placeholder { color: #B0B0B0;font-size:0.18rem;}
+  .input::-webkit-input-placeholder { color: #B0B0B0;font-size:0.18rem;padding-left:0.068rem;}
 </style>
