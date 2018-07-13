e9123a0b75d775ad5d01fbadbe5c0fee75063af7
ShengzheWang <24320162202917@stu.xmu.edu.cn>
2018-07-13 14:13:56 +0800

    最终版

--- AIWeb/src/Controller/ControllerUseList.vue
+++ AIWeb/src/Controller/ControllerUseList.vue
@@ -48,14 +48,9 @@
           <el-form-item style="width: 50%" label="应用名称：">
             <el-input v-model="formChoose.name" style="width: 70%;"></el-input>
           </el-form-item>
-          <el-form-item style="width: 50%" label="应用类型：">
-            <el-select v-model="formChoose.APItype" style="width: 70%;">
-              <el-option v-for="item in options" :key="item.value" :value="item.value" :label="item.name"></el-option>
-            </el-select>
-          </el-form-item>
           <el-form-item label="付费选择：">
             <el-select v-model="formChoose">
-              <el-option value="1" label="付费"></el-option>
+              <!--<el-option value="1" label="付费"></el-option>-->
               <el-option value="2" label="免费"></el-option>
             </el-select>
           </el-form-item>
@@ -118,7 +113,7 @@
         <span style="margin-left: 0.05rem">应用平台</span>
           <div style="height: 0.1rem"></div>
           <div>
-            <el-checkbox-group v-model="formTypes.servers">
+            <el-checkbox-group v-model="formTypes.platform">
               <el-checkbox label="ANDROID" value="1"></el-checkbox>
               <el-checkbox label="IOS" value="2"></el-checkbox>
               <el-checkbox label="WINDOWS" value="3"></el-checkbox>
@@ -131,6 +126,95 @@
         <div style="height: 0.1rem;"></div>
         <span  style="margin-left: 0.05rem">应用描述</span>
           <el-input type="textarea" :rows="8" v-model="description"></el-input>
+        <div style="height: 0.15rem"></div>
+        <div style="width: 100%;text-align: center">
+          <el-button type="primary" @click="ConfirmAPI">确定</el-button>
+          <el-button type="danger" @click="CancelAPI">取消</el-button>
+        </div>
+      </el-dialog>
+      <el-dialog class="index1" :visible.sync="dialogVisible1" :modal-append-to-body="false" title="查看API" style="width: 17rem">
+        <el-form >
+          <el-form-item style="width: 50%" label="应用名称：">
+            <el-input disabled v-model="APIName" style="width: 70%;"></el-input>
+          </el-form-item>
+          <el-form-item label="付费选择：">
+            <el-select v-model="formChoose">
+              <!--<el-option value="1" label="付费"></el-option>-->
+              <el-option value="2" label="免费"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <span style="font-size: 0.25rem;font-weight: bold;margin-left: 0.03rem">接口选择：</span>
+        <div style="height: 0.1rem"></div>
+        <el-card>
+          <span style="font-size: 0.18rem;margin-left: 0.05rem">自然语言处理</span>
+          <div style="margin-top: 0.1rem">
+            <el-checkbox-group v-model="types">
+              <el-checkbox label="语义相似度" value="1"></el-checkbox>
+              <el-checkbox label="短文本相似度" value="2"></el-checkbox>
+              <el-checkbox label="评论观点提取" value="3"></el-checkbox>
+              <el-checkbox label="情感倾向分析" value="4"></el-checkbox>
+              <el-checkbox label="AI客服" value="5"></el-checkbox>
+            </el-checkbox-group>
+          </div>
+          <div style="height: 0.1rem"></div>
+          <span style="font-size: 0.18rem;margin-left: 0.05rem;">语音识别</span>
+          <div style="margin-top: 0.1rem">
+            <el-checkbox-group v-model="types">
+              <el-checkbox label="语音识别" value="6"></el-checkbox>
+              <el-checkbox label="语音合成" value="7"></el-checkbox>
+            </el-checkbox-group>
+          </div>
+          <div style="height: 0.1rem"></div>
+          <span style="font-size: 0.18rem;margin-left: 0.05rem">人脸识别</span>
+          <div style="margin-top: 0.1rem">
+            <el-checkbox-group v-model="types">
+              <el-checkbox label="人脸识别" value="8"></el-checkbox>
+              <el-checkbox label="人脸对比" value="9"></el-checkbox>
+              <el-checkbox label="人脸查找" value="10"></el-checkbox>
+            </el-checkbox-group>
+          </div>
+          <div style="height: 0.1rem"></div>
+          <span style="font-size: 0.18rem;margin-left: 0.05rem">文字识别</span>
+          <div style="margin-top: 0.1rem">
+            <el-checkbox-group v-model="types">
+              <el-checkbox label="通用文字识别" value="11"></el-checkbox>
+              <el-checkbox label="手写识别" value="12"></el-checkbox>
+              <el-checkbox label="身份证识别" value="13"></el-checkbox>
+              <el-checkbox label="银行卡识别" value="14"></el-checkbox>
+              <el-checkbox label="车牌识别" value="15"></el-checkbox>
+              <el-checkbox label="驾驶证识别" value="16"></el-checkbox>
+              <el-checkbox label="行驶证识别" value="17"></el-checkbox>
+            </el-checkbox-group>
+          </div>
+          <div style="height: 0.1rem"></div>
+          <span style="font-size: 0.18rem;margin-left: 0.05rem">图像识别</span>
+          <div style="margin-top: 0.1rem">
+            <el-checkbox-group v-model="types">
+              <el-checkbox label="通用图像分析" value="18"></el-checkbox>
+              <el-checkbox label="去模糊化" value="19"></el-checkbox>
+              <el-checkbox label="人像识别" value="20"></el-checkbox>
+              <el-checkbox label="车型识别" value="21"></el-checkbox>
+            </el-checkbox-group>
+          </div>
+        </el-card>
+        <div style="height: 0.1rem;"></div>
+        <span style="margin-left: 0.05rem">应用平台</span>
+        <div style="height: 0.1rem"></div>
+        <div>
+          <el-checkbox-group v-model="platform">
+            <el-checkbox label="ANDROID" value="1"></el-checkbox>
+            <el-checkbox label="IOS" value="2"></el-checkbox>
+            <el-checkbox label="WINDOWS" value="3"></el-checkbox>
+            <el-checkbox label="HTML5" value="4"></el-checkbox>
+            <el-checkbox label="JAVA" value="5"></el-checkbox>
+            <el-checkbox label="FLASH" value="6"></el-checkbox>
+            <el-checkbox label="LINUX" value="7"></el-checkbox>
+          </el-checkbox-group>
+        </div>
+        <div style="height: 0.1rem;"></div>
+        <span  style="margin-left: 0.05rem">应用描述</span>
+        <el-input type="textarea" :rows="8" v-model="APIdescription"></el-input>
         <div style="height: 0.15rem"></div>
         <div style="width: 100%;text-align: center">
           <el-button type="primary" @click="ConfirmAPI">确定</el-button>
@@ -152,7 +236,7 @@
           <el-table-column prop="status" label="状态"></el-table-column>
           <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+            <el-button @click="ForMore(scope.row)" type="text" size="small">查看</el-button>
             <el-button type="text" size="small" @click="deleteAPI(scope.$index)">删除</el-button>
           </template>
           </el-table-column>
@@ -168,6 +252,11 @@
     name:"ControllerUseList",
     data() {
       return {
+        dialogVisible1:false,
+        APIName:'',
+        types:[],
+        platform:[],
+        APIdescription:'',
         options:[
           {
             name:'游戏娱乐',
@@ -190,7 +279,7 @@
         },
         formTypes:{
           languages:[],
-          servers:[]
+          platform:[]
         },
         description:'',
         rulesChoose:{
@@ -199,11 +288,270 @@
       };
     },
     methods: {
+      ForMore(item){
+        let _this=this;
+
+        this.$axios({
+          method:'get',
+          url:'/api/getApiMore',
+          params:{
+            apikey:item.apikey
+          }
+        }).then(function (response) {
+          _this.$data.APIName=response.data.name;
+          _this.$data.types=response.data.apitype;
+          _this.$data.platform=response.data.platform;
+          _this.$data.APIdescription=response.data.describe;
+          for(let index=0;index<_this.$data.types.length;index++) {
+            switch (_this.$data.types[index]) {
+              case 1:
+                _this.$data.types[index] = '语义相似度';
+                break;
+              case 2:
+                _this.$data.types[index] = '短文本相似度';
+                break;
+              case 3:
+                _this.$data.types[index] = '评论观点抽取';
+                break;
+              case 4:
+                _this.$data.types[index] = '情感倾向分析';
+                break;
+              case 5:
+                _this.$data.types[index] = 'AI客服';
+                break;
+              case 6:
+                _this.$data.types[index] = '语音识别';
+                break;
+              case 7:
+                _this.$data.types[index] = '语音合成';
+                break;
+              case 8:
+                _this.$data.types[index] = '人脸识别';
+                break;
+              case 9:
+                _this.$data.types[index] = '人脸对比';
+                break;
+              case 10:
+                _this.$data.types[index] = '人脸查找';
+                break;
+              case 11:
+                _this.$data.types[index] = '通用文字识别';
+                break;
+              case 12:
+                _this.$data.types[index] = '手写识别';
+                break;
+              case 13:
+                _this.$data.types[index] = '身份证识别';
+                break;
+              case 14:
+                _this.$data.types[index] = '银行卡识别';
+                break;
+              case 15:
+                _this.$data.types[index] = '车牌识别';
+                break;
+              case 16:
+                _this.$data.types[index] = '驾驶证识别';
+                break;
+              case 17:
+                _this.$data.types[index] = '行驶证识别';
+                break;
+              case 18:
+                _this.$data.types[index] = '通用图像分析';
+                break;
+              case 19:
+                _this.$data.types[index] = '去模糊化';
+                break;
+              case 20:
+                _this.$data.types[index] = '人重识别';
+                break;
+              case 21:
+                _this.$data.types[index] = '车型识别';
+                break;
+
+            }
+          }
+
+          for(let index=0;index<_this.$data.platform.length;index++){
+            switch(_this.$data.platform[index]){
+              case 1:
+                _this.$data.platform[index]='ANDROID';
+                break;
+              case 2:
+                _this.$data.platform[index]='IOS';
+                break;
+              case 3:
+                _this.$data.platform[index]='WINDOWS';
+                break;
+              case 4:
+                _this.$data.platform[index]='HTML5';
+                break;
+              case 5:
+                _this.$data.platform[index]='JAVA';
+                break;
+              case 6:
+                _this.$data.platform[index]='FLASH';
+                break;
+              case 7:
+                _this.$data.platform[index]='LINUX';
+                break;
+            }
+          }
+
+          console.log(_this.$data.types);
+          _this.dialogVisible1=true;
+        })
+
+
+      },
+
+      /**
+       * 提交新API
+       * @constructor
+       */
       ConfirmAPI(){
+        let types=this.$data.formTypes;
+        let stringtypes=''
+        for(let index=0;index<types.languages.length;index++){
+          switch(types.languages[index]){
+            case '语义相似度':
+              types.languages[index]='1';
+              break;
+            case '短文本相似度':
+              types.languages[index]='2';
+              break;
+            case '评论观点提取':
+              types.languages[index]='3';
+              break;
+            case '情感倾向分析':
+              types.languages[index]='4';
+              break;
+            case 'AI客服':
+              types.languages[index]='5';
+              break;
+            case '语音识别':
+              types.languages[index]='6';
+              break;
+            case '语音合成':
+              types.languages[index]='7';
+              break;
+            case '人脸识别':
+              types.languages[index]='8';
+              break;
+            case '人脸对比':
+              types.languages[index]='9';
+              break;
+            case '人脸查找':
+              types.languages[index]='10';
+              break;
+            case '通用文字识别':
+              types.languages[index]='11';
+              break;
+            case '手写识别':
+              types.languages[index]='12';
+              break;
+            case '身份证识别':
+              types.languages[index]='13';
+              break;
+            case '银行卡识别':
+              types.languages[index]='14';
+              break;
+            case '车牌识别':
+              types.languages[index]='15';
+              break;
+            case '驾驶证识别':
+              types.languages[index]='16';
+              break;
+            case '行驶证识别':
+              types.languages[index]='17';
+              break;
+            case '通用图像分析':
+              types.languages[index]='18';
+              break;
+            case '去模糊化':
+              types.languages[index]='19';
+              break;
+            case '人像识别':
+              types.languages[index]='20';
+              break;
+            case '车型识别':
+              types.languages[index]='21';
+              break;
+          }
+          if(index===types.languages.length-1) {
+            stringtypes += types.languages[index];
+          }else{
+            stringtypes=stringtypes+ types.languages[index]+',';
+          }
+          }
+          console.log(stringtypes);
+
+
+        let stringplatform='';
+        for(let index=0;index<types.platform.length;index++) {
+          switch (types.platform[index]) {
+            case 'ANDROID':
+              types.platform[index] = '1'
+              break;
+            case 'IOS':
+              types.platform[index] = '2';
+              break;
+            case 'WINDOWS':
+              types.platform[index] = '3';
+              break;
+            case 'HTML5':
+              types.platform[index] = '4';
+              break;
+            case 'JAVA':
+              types.platform[index] = '5';
+              break;
+            case 'FLASH':
+              types.platform[index] = '6';
+              break;
+            case 'LINUX':
+              types.platform[index] = '7';
+              break;
+          }
+
+          if(index===types.platform.length-1){
+            stringplatform+=types.platform[index];
+          }else {
+            stringplatform = stringplatform+ types.platform[index] + ','
+          }
+        }
+
+        console.log(stringplatform)
+
+        let temp=document.cookie.split(';')
+        let account=temp[0].split('=')[1];
+
+        let _this=this;
+
+        this.$axios({
+          method:'get',
+          url:'/api/newApi',
+          params:{
+            account:account,
+            type:'7,13',
+            name:this.$data.formChoose.name,
+            platform:'4',
+            describe:this.$data.description
+          }
+        }).then(function (response) {
+          if(response.data===true){
+            alert('提交成功！');
+            _this.dialogVisible=false;
+            _this.$router.push('/Controller/ControllerSpeachAPI')
+          }else{
+            alert('提交失败')
+          }
+        }).catch(function (err) {
+          alert('提交失败')
+        })
+
 
       },
       CancelAPI(){
-
+        this.dialogVisible=false;
       },
       handleOpen(key, keyPath) {
         console.log(key, keyPath);
@@ -233,13 +581,15 @@
       }
     },
     created(){
+      let temp=document.cookie.split(';')
+      let account=temp[0].split('=')[1];
       let _this=this;
       this.$axios({
         method:'get',
         url:'/api/getApi',
         params:{
-          account:'123456',
-          page:'1'
+          account:account,
+          page:1
         }
       }).then(function (response) {
         _this.$data.manifests=response.data.apiVos;
